eea11221a5c13c0431ed1b8ef573c5d4b6147e96
+++ATH0 <1099006+WeaponizedAutismDev@users.noreply.github.com>
2024-10-29 00:42:18 +0800

    feat: update icon picker and choices

--- Telegram/SourceFiles/ayu/ui/ayu_logo.h
+++ Telegram/SourceFiles/ayu/ui/ayu_logo.h
@@ -12,16 +12,34 @@ namespace AyuAssets {
 
 ICON(DEFAULT, "default");
 ICON(DEFAULT_MACOS, "macos");
-ICON(ALT, "alt");
-ICON(DISCORD, "discord");
-ICON(SPOTIFY, "spotify");
-ICON(EXTERA, "extera");
-ICON(NOTHING, "nothing");
-ICON(BARD, "bard");
-ICON(YAPLUS, "yaplus");
-ICON(WIN95, "win95");
-ICON(CHIBI, "chibi");
-ICON(CHIBI2, "chibi2");
+// ICON(ALT, "alt");
+// ICON(DISCORD, "discord");
+// ICON(SPOTIFY, "spotify");
+// ICON(EXTERA, "extera");
+// ICON(NOTHING, "nothing");
+// ICON(BARD, "bard");
+// ICON(YAPLUS, "yaplus");
+// ICON(WIN95, "win95");
+// ICON(CHIBI, "chibi");
+// ICON(CHIBI2, "chibi2");
+ICON(YS, "ys");
+ICON(YR, "yr");
+ICON(YNP, "ynp");
+ICON(RR, "rr");
+ICON(RS, "rs");
+ICON(RNP, "rnp");
+ICON(GS, "gs");
+ICON(GR, "gr");
+ICON(GNP, "gnp");
+ICON(TR, "tr");
+ICON(TS, "ts");
+ICON(TNP, "tnp");
+ICON(PR, "pr");
+ICON(PS, "ps");
+ICON(PNP, "pnp");
+ICON(PRNP, "prnp");
+ICON(PRR, "prr");
+ICON(PRS, "prs");
 
 void loadAppIco();
 
